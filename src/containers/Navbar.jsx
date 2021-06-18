c4d473bd50374b2a35af919426c7a9ac5e0c8b70
Ashish Dev <ashishdevofficial@outlook.com>
2021-06-17 22:17:54 -0500

    dummy search for search bar

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
 import '../style/Navbar.css';
 import logo from '../assets/logo.png';
 import gps from '../assets/gps.svg';
 
 const Navbar = () => {
+  const history = useHistory();
+
   return (
     <div class="topnav">
       <img id="logo" src={logo} alt="logo" />
@@ -14,17 +18,25 @@ const Navbar = () => {
           <div>Select Your Address</div>
         </div>
       </div>
-      <div class="ui left action input right icon" id="product-search">
-        <select class="ui compact selection dropdown">
-          <option value="all">All</option>
-          <option selected="" value="articles">
-            Articles
-          </option>
-          <option value="products">Products</option>
-        </select>
-        <input type="text" placeholder="Search..." />
-        <i class="search icon"></i>
-      </div>
+      <form
+        id="product-search"
+        onSubmit={(e) => {
+          e.preventDefault();
+          history.push('/products/drill');
+        }}
+      >
+        <div class="ui left action input right icon" style={{ width: '100%' }}>
+          <select class="ui compact selection dropdown">
+            <option value="all">All</option>
+            <option selected="" value="articles">
+              Articles
+            </option>
+            <option value="products">Products</option>
+          </select>
+          <input type="text" placeholder="Search..." />
+          <i class="search icon"></i>
+        </div>
+      </form>
       <a href="login">Login</a>
     </div>
   );
